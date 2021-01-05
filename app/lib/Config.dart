fd3edf8ed3c343e1c6692e5282b0fd787acaf95e
Felix Weuthen <felix@sharezone.info>
2021-01-05 18:38:19 +0100

    updates to pub packages + ios

--- app/lib/Config.dart
+++ app/lib/Config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static String versionName = '10.3.1';
-  static int versionCode = 614;
+  static String versionName = '10.3.2';
+  static int versionCode = 615;
 
   //ignore: avoid_init_to_null
   static String uid = null;
